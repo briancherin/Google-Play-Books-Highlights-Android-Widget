664372a3fc275360b8f76f386f10939b163f02d6
Brian Cherin <brian.cherin@gmail.com>
2023-06-25 10:33:42 -0400

    Add date, book link, and note button (no note text yet)

--- app/src/main/java/com/corson/playbookshighlightswidget/higlights_recycler_view/BookHighlightsAdapter.kt
+++ app/src/main/java/com/corson/playbookshighlightswidget/higlights_recycler_view/BookHighlightsAdapter.kt
@@ -1,11 +1,16 @@
 package com.corson.playbookshighlightswidget.higlights_recycler_view
 
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.corson.playbookshighlightswidget.R
 import com.corson.playbookshighlightswidget.model.Highlight
@@ -15,12 +20,18 @@ class BookHighlightsAdapter(private val highlightsList: ArrayList<Highlight>) :
         RecyclerView.Adapter<BookHighlightsAdapter.ViewHolder>() {
 
         class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-                val textView: TextView
+                val highlightTextTextView: TextView
                 val cardView: CardView
+                val dateTextView: TextView
+                val noteButton: ImageButton
+                val linkButton: ImageButton
 
                 init {
-                        textView = view.findViewById(R.id.bookHighlightRowTextView)
+                        highlightTextTextView = view.findViewById(R.id.bookHighlightRowTextView)
                         cardView = view.findViewById(R.id.bookHighlightRowCardView)
+                        dateTextView = view.findViewById(R.id.bookHighlightRowDate)
+                        noteButton = view.findViewById(R.id.bookHighlightRowNotesButton)
+                        linkButton = view.findViewById(R.id.bookHighlightRowLinkButton)
                 }
 
         }
@@ -33,8 +44,25 @@ class BookHighlightsAdapter(private val highlightsList: ArrayList<Highlight>) :
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-                holder.textView.text = highlightsList[position].quoteText
-                holder.cardView.setCardBackgroundColor(Color.parseColor(highlightsList[position].highlightColor))
+                val highlight = highlightsList[position]
+
+                holder.highlightTextTextView.text = highlight.quoteText
+                holder.cardView.setCardBackgroundColor(Color.parseColor(highlight.highlightColor))
+                holder.dateTextView.text = highlight.dateHighlighted
+
+                if (highlight.highlightNotes != null && highlight.highlightNotes!!.isNotEmpty()) {
+                        holder.noteButton.visibility = View.VISIBLE
+                } else {
+                        holder.noteButton.visibility = View.GONE
+                }
+
+                holder.linkButton.setOnClickListener {
+                        startActivity(
+                                holder.itemView.context,
+                                Intent(Intent.ACTION_VIEW, Uri.parse(highlight.bookLink)),
+                                null
+                        )
+                }
         }
 
         override fun getItemCount(): Int {
